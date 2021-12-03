e4e1807e3e0c100fe17331934a9cbe2445f7e9a4
Matheus Amaral <Matheus2207amaral@gmail.com>
2021-12-03 15:34:16 -0300

    codigo

---
+++ model.py
@@ -0,0 +1,13 @@
+def pintura():
+    nome=input("digite o nome do avião")
+    metros=float(input("digite a quantidade de metros quadrados"))
+    latas=metros/11
+    print("para pintar o avião {} será necessário {:.2f} latas de tinta".format(nome,latas))
+
+
+
+
+
+def instrucoes():
+    print("para usar este programa você deverá informar o nome do avião e quantos metros quadrados ele possui para calcular quantas latas de tinta será necessário para pintar o avião")
+    
